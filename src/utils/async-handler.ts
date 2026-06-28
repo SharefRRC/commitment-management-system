12f9ee06877e763045f7dade67fb83a026d7c751
SharefRRC <sislam48@academic.rrc.ca>
2026-06-28 18:42:07 -0500

    commit with minor fixes and passing tests

--- src/utils/async-handler.ts
+++ src/utils/async-handler.ts
@@ -1,8 +1,24 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
+import { ParamsDictionary } from "express-serve-static-core";
+import { ParsedQs } from "qs";
 
-export const asyncHandler =
-  (
-    fn: (req: Request, res: Response, next: NextFunction) => Promise<unknown>
-  ) =>
-  (req: Request, res: Response, next: NextFunction) =>
+export const asyncHandler = <
+  P = ParamsDictionary,
+  ResBody = any,
+  ReqBody = any,
+  ReqQuery = ParsedQs
+>(
+  fn: (
+    req: Request<P, ResBody, ReqBody, ReqQuery>,
+    res: Response<ResBody>,
+    next: NextFunction
+  ) => Promise<any>
+): RequestHandler<P, ResBody, ReqBody, ReqQuery> => {
+  return (
+    req: Request<P, ResBody, ReqBody, ReqQuery>,
+    res: Response<ResBody>,
+    next: NextFunction
+  ) => {
     Promise.resolve(fn(req, res, next)).catch(next);
+  };
+};
